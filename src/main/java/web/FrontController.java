d664b45c3e7b56473d72ca852ecc9e32ad345d11
MagnusGranno <70953393+MagnusGranno@users.noreply.github.com>
2021-04-19 11:48:12 +0200

    fixed header

--- src/main/java/web/FrontController.java
+++ src/main/java/web/FrontController.java
@@ -18,8 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet
 {
     private final static String USER = "dev";
-    private final static String PASSWORD = "ax2";
-    private final static String URL = "jdbc:mysql://localhost:3306/startcode?serverTimezone=CET";
+    private final static String PASSWORD = "Dev2139!";
+    private final static String URL = "jdbc:mysql://localhost:3306/cupcake?serverTimezone=CET";
 
     public static Database database;
 
