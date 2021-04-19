e4626db804349075a00f8b93fc90951a12a41450
MagnusGranno <70953393+MagnusGranno@users.noreply.github.com>
2021-04-19 14:17:58 +0200

    toppingmapper

--- src/main/java/business/entities/Topping.java
+++ src/main/java/business/entities/Topping.java
@@ -1,2 +1,31 @@
-package business.entities;public class Topping {
+package business.entities;
+
+
+public class Topping
+{
+    private int id;
+    private String name;
+    private int price;
+
+    public Topping(int id, String name, int price)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
 }
--- src/main/java/business/persistence/ToppingMapper.java
+++ src/main/java/business/persistence/ToppingMapper.java
@@ -1,2 +1,53 @@
-package business.persistence;public class ToppingMapper {
+package business.persistence;
+
+import business.entities.Bottom;
+import business.entities.Topping;
+import business.exceptions.UserException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToppingMapper
+{
+    private Database database;
+
+    public ToppingMapper(Database database)
+    {
+        this.database = database;
+    }
+
+    public List<Topping> getAllToppings() throws UserException
+    {
+        List<Topping> toppingList = new ArrayList<>();
+        try (Connection connection = database.connect())
+        {
+            String sql = "SELECT * FROM topping;";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ResultSet rs = ps.executeQuery();
+                while(rs.next())
+                {
+                    int id = rs.getInt("topping_id");
+                    String name = rs.getString("name");
+                    int price = rs.getInt("price");
+
+                    toppingList.add(new Topping(id, name, price));
+                }
+                return toppingList;
+            }
+            catch (SQLException ex)
+            {
+                throw new UserException(ex.getMessage());
+            }
+        }
+        catch(SQLException ex)
+        {
+            throw new UserException(ex.getMessage());
+        }
+    }
 }
